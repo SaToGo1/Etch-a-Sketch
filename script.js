f9ad823881921c1c82bdc0685211bd5b330dadeb
SaToGo <stoledano97@gmail.com>
2022-03-15 01:52:23 +0100

    Corrected the default size of the grid and its elements

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Creates the grid in which we will draw.
-function gridCreation(numberOfCells = 16) {
+function gridCreation(numberOfCells = 16*16) {
     const gridContainer = document.querySelector(".gridContainer");
     for(let i = 0; i < numberOfCells; i++){
         const div = document.createElement('div');
