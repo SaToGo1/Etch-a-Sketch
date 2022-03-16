d38ad60ab4995c1cb204d8074255fd82646ae7a4
SaToGo <stoledano97@gmail.com>
2022-03-17 00:01:26 +0100

    Modified gridCreation and added calculateSizeOfGridBody to make the grid adjust to the gridContainer size when we add more gridBody elements

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+const GRID_SIZE = 500;
+
+//Creates grid at the load of the page
 const body = document.querySelector("body");
-body.onload = function(){ gridCreation()} ;
+body.onload = function(){ gridCreation()};
 
 //Creates the grid in which we will draw.
 function gridCreation(numberOfCells = 16) {
     gridDelete();
     const gridContainer = document.querySelector(".gridContainer");
+    let pixel = calculateSizeOfGridBody(numberOfCells);
     for(let i = 0; i < numberOfCells; i++){
         for(let j = 0; j < numberOfCells; j++){
             const div = document.createElement('div');
             div.setAttribute('class', 'gridBody');
             div.setAttribute('onmouseover', 'putColor(this)');
+            div.style.height = `${pixel}px`;
+            div.style.width = `${pixel}px`;
             gridContainer.appendChild(div);
         }
     }
@@ -18,6 +24,11 @@ function gridCreation(numberOfCells = 16) {
 function gridDelete() {
     const gridContainer = document.querySelector(".gridContainer");
     gridContainer.innerHTML = "";
+}
+
+function calculateSizeOfGridBody(numberOfCells) {
+    // 2 from the border
+    return (GRID_SIZE/numberOfCells)-2;
 }
 
 function putColor(pixel){
